197474944d0d265b1979b739f3b9dea9596f121f
Xatzel <xatzel00@gmail.com>
2019-06-21 12:51:36 -0500

    se agrego signIn signout y logIn

--- react-firebase-authentication/src/components/SignUp/index.js
+++ react-firebase-authentication/src/components/SignUp/index.js
@@ -1,9 +1,108 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { compose } from "recompose";
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constans/routes";
 
-const SignUp = () => (
+const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
 
-export default SignUp;
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+
+class SignUpFormBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { username, email, passwordOne } = this.state;
+
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === "" ||
+      email === "" ||
+      username === "";
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Nombre"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Dirección de correo"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Contraseña"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirma tu contraseña"
+        />
+        <button disabled={isInvalid} type="submit">
+          Registrar
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+const SignUpLink = () => (
+  <p>
+    ¿No tienes una cuenta? <Link to={ROUTES.SIGN_UP}>Regístrate</Link>
+  </p>
+);
+//Función de anidamiento (paracomponentes de orden superior) para comunicarse entre sí.
+//"Compose" aplica a los componentesde orden superios de derecha a izquierda
+const SignUpForm = compose(
+  withRouter,
+  withFirebase
+)(SignUpFormBase);
+
+// const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+export default SignUpPage;
+export { SignUpForm, SignUpLink };
